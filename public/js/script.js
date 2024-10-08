30979e00a5351146104447e21d12e0a5c87b783a
Pham Tien Dat <tdat9663@gmail.com>
2024-10-08 16:58:06 +0700

    alert success

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,21 @@
+//alert add to cart
+
+const alertAddCartSuccess = () => {
+  const elementAlert = document.querySelector('[alert-add-cart-susscess]')
+  if (elementAlert) {
+    elementAlert.classList.remove('alert-hidden')
+
+    setTimeout(() => {
+      elementAlert.classList.add('alert-hidden')
+    }, 2000)
+  }
+}
+// end alert add to cart
+
+
 // tour-images
 const tourImages = document.querySelector(".tour-images");
-if(tourImages) {
+if (tourImages) {
   const swiper = new Swiper(".tour-images", {});
 }
 // End tour-images
@@ -9,33 +24,31 @@ if(tourImages) {
 
 // cart variable nay chỉ được khởi tạo 1 lần duy nhất
 // khi submit thì sẽ k còn nữa
-const cart=localStorage.getItem('cart');
-if(!cart){
-  localStorage.setItem('cart',JSON.stringify([]));   
+const cart = localStorage.getItem('cart');
+if (!cart) {
+  localStorage.setItem('cart', JSON.stringify([]));
 }
-const formAddToCart=document.querySelector('[form-add-to-cart]')
-if(formAddToCart){
-  formAddToCart.addEventListener('submit',e=>{
+const formAddToCart = document.querySelector('[form-add-to-cart]')
+if (formAddToCart) {
+  formAddToCart.addEventListener('submit', e => {
     e.preventDefault();
-    const idTour=parseInt(formAddToCart.getAttribute('tour-id'))
-    const quantity=parseInt(formAddToCart.quantity.value)
-    if(idTour&&quantity>0){
+    const idTour = parseInt(formAddToCart.getAttribute('tour-id'))
+    const quantity = parseInt(formAddToCart.quantity.value)
+    if (idTour && quantity > 0) {
       // dua ve js
-      const cart=JSON.parse(localStorage.getItem('cart'));
+      const cart = JSON.parse(localStorage.getItem('cart'));
 
       // ktra da ton tai tour chua
-      const existTour=cart.find(tour=>tour.id==idTour)
-      if(existTour){
-        existTour.quantity+=quantity
-      }
-      else 
+      const existTour = cart.find(tour => tour.id == idTour)
+      if (existTour) {
+        existTour.quantity += quantity
+      } else
         cart.push({
-          id:idTour,
-          quantity:quantity
-      })
-
-      console.log(cart);
-      localStorage.setItem('cart',JSON.stringify(cart))
+          id: idTour,
+          quantity: quantity
+        })
+      localStorage.setItem('cart', JSON.stringify(cart))
+      alertAddCartSuccess();
     }
   })
 }
