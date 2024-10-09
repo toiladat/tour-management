4fa1454e7a9f3178307fe33689031b8319a44f21
Pham Tien Dat <tdat9663@gmail.com>
2024-10-09 16:31:26 +0700

    lưu thông tin tours vào orders_item

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,6 +1,8 @@
 import { Request,Response } from "express"
 import Order from "../../models/order.models"
 import {generateOrderCode} from '../../helpers/generate.helpers'
+import Tour from "../../models/tour.models"
+import OrderItem from "../../models/order-item.model"
 export const index =async(req:Request,res:Response)=>{
   const data=req.body
   //dữ liệu lưu trong bảng orders
@@ -16,7 +18,7 @@ export const index =async(req:Request,res:Response)=>{
   // cập nhật trường code cho bản ghi
   let orderId= order.dataValues.id;
   const code=generateOrderCode(orderId)
-  
+
   await Order.update({
     code:code
   },{
@@ -24,6 +26,26 @@ export const index =async(req:Request,res:Response)=>{
       id:orderId
     }
   })
+  // lưu thông tin vào bảng orders_item
+  for( const tour of data.cart){
+    const order_item_data={
+      orderId:orderId,
+      tourId:tour.id,
+      quantity:tour.quantity
+    }
+
+    const tourInfor= await Tour.findOne({
+      where:{
+        id:tour.id
+      }
+    })
+    order_item_data['price']=tourInfor['price']
+    order_item_data['discount']=tourInfor['discount']
+    order_item_data['timeStart']=tourInfor['timeStart']
+
+    await OrderItem.create(order_item_data)
+    
+  }
 
   res.json({
     message:"Đặt hàng thành công",
